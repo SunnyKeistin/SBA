80ed8f191cc1f90e49b4457fea72aba805716eec
Kai Qin Chen <chenkaiq@cn.ibm.com>
2020-04-02 09:40:27 +0800

    commit git

--- sba-frontend/e2e/src/app.e2e-spec.ts
+++ sba-frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('fsd-frontend app is running!');
+    expect(page.getTitleText()).toEqual('sba-frontend app is running!');
   });
 
   afterEach(async () => {
--- sba-frontend/src/app/app.component.spec.ts
+++ sba-frontend/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fsd-frontend'`, () => {
+  it(`should have as title 'sba-frontend'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fsd-frontend');
+    expect(app.title).toEqual('sba-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('fsd-frontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('sba-frontend app is running!');
   });
 });
